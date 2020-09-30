45a0e34bb2169eab3a03195f188adef7da4542ee
NguyenDinhHuong30088 <70881463+NguyenDinhHuong30088@users.noreply.github.com>
2020-09-30 09:23:41 +0700

    BTVN7

---
+++ BTVN7/BTVN7_3.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf ("Nhap n: ");
+	scanf ( "%d",&n);
+	int ar [n];
+	for ( int i = 0 ; i <n; i ++) {
+		int x;
+		flag bool ;
+	
+		do{
+			flag = false ;
+			printf ( " Nhap ar [% d]: " , i);
+			scanf ( " % d " , & x);
+			for ( int j = 0 ; j <i; j ++) {
+				if (ar [j] == x) {
+					flag = true ;
+					break;
+				}
+			}
+			if (flag == false ) {
+				ar [i] = x;
+			}
+			
+		} while (flag);
+	}
+	printf ( " Mang da nhap la: \ n " );
+	for ( int i = 0 ; i <n; i ++) {
+		printf ( " % d \ t " , ar [i]);
+	}
+}
